832610058c3399ecb9fa37eade981be63702f0b9
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-02 08:23:00 +0100

    add dev tools + type hints

--- 2022/day01/elves.py
+++ 2022/day01/elves.py
@@ -2,19 +2,20 @@ import pathlib
 
 
 class ElfCalories:
-    def __init__(self, calories_input: pathlib.Path):
+
+    def __init__(self, calories_input: pathlib.Path) -> None:
         content = calories_input.read_text()
         self.elf_calories = content.split("\n\n")
         self.total_kcal_per_elf = sorted(self._calculate_kcal_per_elf())
 
-    def _calculate_kcal_per_elf(self):
+    def _calculate_kcal_per_elf(self) -> list[int]:
         total_kcal_per_elf = []
         for calorie_line in self.elf_calories:
             total_kcal_per_elf.append(sum(int(i) for i in calorie_line.split()))
         return total_kcal_per_elf
 
-    def get_max_kcal_elves(self):
+    def get_max_kcal_elves(self) -> int:
         return self.total_kcal_per_elf[-1]
 
-    def get_total_kcal_top_elves(self, n=3):
+    def get_total_kcal_top_elves(self, n: int = 3) -> int:
         return sum(self.total_kcal_per_elf[-n:])
