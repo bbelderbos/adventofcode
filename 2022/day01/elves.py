b306078fd55fbdc33e7b6623b854466159a357b1
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-02 07:55:43 +0100

    don't have to keep track of elf keys, only values

--- 2022/day01/elves.py
+++ 2022/day01/elves.py
@@ -5,10 +5,12 @@ elf_calories = content.split("\n\n")
 
 
 def _calculate_kcal_per_elf():
-    total_kcal_per_elf = {}
-    for elf, calorie_line in enumerate(elf_calories, start=1):
-        total_kcal_per_elf[elf] = sum(
-            int(i) for i in calorie_line.split()
+    total_kcal_per_elf = []
+    for calorie_line in elf_calories:
+        total_kcal_per_elf.append(
+            sum(
+                int(i) for i in calorie_line.split()
+            )
         )
     return total_kcal_per_elf
 
@@ -17,13 +19,9 @@ total_kcal_per_elf = _calculate_kcal_per_elf()
 
 
 def get_max_kcal_elves():
-    return max(
-        total_kcal_per_elf.items(), key=lambda x: x[1]
-    )[1]
+    return max(total_kcal_per_elf)
 
 
 def get_total_kcal_top_elves(n=3):
-    top_elves = sorted(
-        total_kcal_per_elf.items(), key=lambda x: x[1], reverse=True
-    )[:n]
-    return sum(x[1] for x in top_elves)
+    top_elves = sorted(total_kcal_per_elf, reverse=True)[:n]
+    return sum(top_elves)
