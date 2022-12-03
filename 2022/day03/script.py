c94c0b627aa5057c2318d9a9e2ace0d2aec53065
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-03 19:10:30 +0100

    got part 2 working

--- 2022/day03/script.py
+++ 2022/day03/script.py
@@ -22,5 +22,29 @@ def get_item(rugsack: str) -> Item:
     return Item(common_item, item_value)
 
 
-def get_total_sum_of_priorities(all_rugsacks):
+def get_total_sum_of_priorities(all_rugsacks: str) -> int:
     return sum(get_item(rugsack.strip()).value for rugsack in all_rugsacks.splitlines())
+
+
+def get_common_item(batch_rugsacks: list[str]) -> str:
+    common = set(batch_rugsacks[0].strip())
+    for rs in batch_rugsacks[1:]:
+        common &= set(rs)
+    return common.pop()
+
+
+def _chunker(seq: list[str], size: int) -> typing.Iterator[list[str]]:
+    """Source: https://stackoverflow.com/a/434328"""
+    # fmt: off
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+    # fmt: on
+
+
+def get_total_group_common_items(all_rugsacks: str, size: int = 3) -> int:
+    rugsacks = all_rugsacks.splitlines()
+    total = 0
+    for chunk in _chunker(rugsacks, size):
+        item = get_common_item(chunk)
+        value = ITEM_PRIORITIES[item]
+        total += value
+    return total
--- 2022/day03/test_script.py
+++ 2022/day03/test_script.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-from script import get_total_sum_of_priorities
+from script import get_total_group_common_items, get_total_sum_of_priorities
 
 BASE_DIR = Path(__file__).parent
 
 
-def test_example_rugsacks():
+def test_example_rugsacks_part1():
     all_rugsacks = """
     vJrwpWtwJgWrhcsFMMfFFhFp
     jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -18,7 +18,25 @@ def test_example_rugsacks():
     assert total == 157
 
 
+def test_example_rugsacks_part2():
+    all_rugsacks = """
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    """.strip()
+    assert get_total_group_common_items(all_rugsacks) == 70
+
+
 def test_part1_solution():
     all_rugsacks = (BASE_DIR / "input.txt").read_text()
     total = get_total_sum_of_priorities(all_rugsacks)
     assert total == 7903
+
+
+def test_part2_solution():
+    all_rugsacks = (BASE_DIR / "input.txt").read_text()
+    total = get_total_group_common_items(all_rugsacks)
+    assert total == 2548
