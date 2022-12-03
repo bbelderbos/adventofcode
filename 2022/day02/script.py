76b7fa17b13afcfc0c915e6a17f1c7a4d34743da
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-03 09:18:11 +0100

    name constants uppercase

--- 2022/day02/script.py
+++ 2022/day02/script.py
@@ -1,5 +1,5 @@
-win, loss, draw = 6, 0, 3
-who_beats_who = dict(
+LOSS, DRAW, WIN = 0, 3, 6
+WHO_BEATS_WHO = dict(
     A="C",  # rock beats scissors
     B="A",  # paper beats rock
     C="B",  # scissors beats paper
@@ -7,28 +7,28 @@ who_beats_who = dict(
     Y="A",  # paper beats rock
     Z="B",  # scissors beats paper
 )
-# x = rock, y = paper, z = scissors
-role_points = dict(A=1, B=2, C=3, X=1, Y=2, Z=3)
-player_aliases = dict(X="A", Y="B", Z="C")
+# a|x = rock, b|y = paper, c|z = scissors
+ROLE_POINTS = dict(A=1, B=2, C=3, X=1, Y=2, Z=3)
+PLAYER_ALIASES = dict(X="A", Y="B", Z="C")
 
 
 def get_game_score(opponent, me):
-    if me in player_aliases:
-        my_rps = player_aliases[me]
+    if me in PLAYER_ALIASES:
+        my_rps = PLAYER_ALIASES[me]
     else:
         my_rps = me
     if opponent == my_rps:
-        return draw
-    defeaten = who_beats_who[me]
+        return DRAW
+    defeaten = WHO_BEATS_WHO[me]
     if opponent == defeaten:
-        return win
-    return loss
+        return WIN
+    return LOSS
 
 
 def get_score_for_round(line):
     opponent, me = line.split()
     game_score = get_game_score(opponent, me)
-    role_score = role_points[me]
+    role_score = ROLE_POINTS[me]
     final_score = game_score + role_score
     # print(line, "->", game_score, role_score, final_score)
     return final_score
@@ -36,11 +36,11 @@ def get_score_for_round(line):
 
 def _determine_role(opponent, me):
     # x y z = lose draw win
-    beaten_by = {v: k for k, v in who_beats_who.items()}
+    beaten_by = {v: k for k, v in WHO_BEATS_WHO.items()}
     if me == "Y":
         return opponent
     elif me == "X":
-        return who_beats_who[opponent]
+        return WHO_BEATS_WHO[opponent]
     else:
         # win
         return beaten_by[opponent]
@@ -50,7 +50,7 @@ def get_score_for_round_part2(line):
     opponent, me = line.split()
     desired_role = _determine_role(opponent, me)
     game_score = get_game_score(opponent, desired_role)
-    role_score = role_points[desired_role]
+    role_score = ROLE_POINTS[desired_role]
     final_score = game_score + role_score
     # print(line, "->", game_score, role_score, final_score)
     return final_score
