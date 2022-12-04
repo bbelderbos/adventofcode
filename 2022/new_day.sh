31e355d887c500c953dd936f5d4c2d87be7c2ce6
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-04 08:30:29 +0100

    new day script

---
+++ 2022/new_day.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 day-num" >&2
+	exit 1
+fi
+
+dir=day$1
+
+mkdir $dir
+touch $dir/input.txt
+touch $dir/script.py
+touch $dir/test_script.py
