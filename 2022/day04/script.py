bce577d7e67d7eda4bf0470073d492cd230f93bb
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-04 09:40:06 +0100

    part 1

---
+++ 2022/day04/script.py
@@ -0,0 +1,23 @@
+from typing import Iterable
+
+
+def _create_section_range(pair: str) -> Iterable:
+    start, end = pair.split("-")
+    return range(int(start), int(end) + 1)
+
+
+def _has_overlap(first: Iterable, second: Iterable) -> bool:
+    return min(first) >= min(second) and max(first) <= max(second)
+
+
+def calculate_number_if_overlaps(pairs: str) -> int:
+    total = 0
+    for pair in pairs.splitlines():
+        first, second = pair.split(",")
+        first_section = _create_section_range(first)
+        second_section = _create_section_range(second)
+        if _has_overlap(first_section, second_section) or _has_overlap(
+            second_section, first_section
+        ):
+            total += 1
+    return total
---
+++ 2022/day04/test_script.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from script import calculate_number_if_overlaps
+
+BASE_DIR = Path(__file__).parent
+
+EXERCISE_SAMPLE = """
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+""".strip()
+
+
+def test_part1_with_sample():
+    assert calculate_number_if_overlaps(EXERCISE_SAMPLE) == 2
+
+
+def test_part1_with_input_file():
+    pairs = (BASE_DIR / "input.txt").read_text()
+    assert calculate_number_if_overlaps(pairs) == 511
