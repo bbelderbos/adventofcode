dc753200a9b096d21b196017329176bbf2273d0f
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-05 18:45:06 +0100

    part 1 works

--- 2022/day05/script.py
+++ 2022/day05/script.py
@@ -1,61 +1,42 @@
 import re
-from pprint import pprint as pp
-
-input_text = "    [D]    \n" "[N] [C]    \n" "[Z] [M] [P]\n" " 1   2   3 \n"
-input_text += """
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2
-"""
 
 Stacks = list[list[str]]
 
 
-def populate_stacks(stack_data: str = input_text) -> Stacks:
+def populate_stacks(stack_data: str) -> Stacks:
     number_of_stacks = int(stack_data.strip()[-1])
 
-    # cannot do [[]] * number_of_stacks (lists with same id!)
-    stacks = []
+    # cannot do [[]] * num_stacks (= by reference / lists with same id!)
+    stacks: Stacks = []
     for _ in range(number_of_stacks):
         stacks.append([])
 
     for line in stack_data.splitlines()[:-1]:
-        crates = re.findall(r"([ ]{3}|\[[A-Z]\])", line)
+        crates = re.findall(r"(?:\s{3}|(\[[A-Z]\]))\s?", line)
+        # argh the spaces after [V] get truncated
+        if len(crates) < number_of_stacks:
+            crates.append("")  # 8 -> 9
+
         for i, crate in enumerate(crates):
-            stacks[i].append(crate)
+            if crate:
+                stacks[i].append(crate)
+
     return stacks
 
 
-stack_data, instructions = input_text.split("\n\n")
-stacks = populate_stacks(stack_data)
-from pprint import pprint as pp
-
-pp(stacks)
-
-
-def move_crates(stacks: Stacks, instructions: str = instructions):
+def move_crates(stacks: Stacks, instructions: str) -> Stacks:
     for instruction in instructions.strip().splitlines():
-        # move 1 from 2 to 1
-        amount, from_, to_ = re.findall(r"\d", instruction)
-        print(amount, from_, to_)
+        amount, from_, to_ = re.findall(r"\d+", instruction)
         for _ in range(int(amount)):
             from_index = int(from_) - 1
             to_index = int(to_) - 1
-            from_crate = stacks[from_index].pop(0)  # TODO: use deque
-            print(from_crate, to_index, "from index", from_index)
-            print("result")
-            # breakpoint()
-            if stacks[to_index] and not stacks[to_index][0].strip():
-                stacks[to_index][0] = from_crate
-                stacks[from_index].insert(0, "   ")
-            else:
-                stacks[to_index].insert(0, from_crate)  # TODO: use deque
-            print(stacks)
-            print()
+            from_crate = stacks[from_index].pop(0)
+            stacks[to_index].insert(0, from_crate)  # TODO: use deque
+    return stacks
 
 
-move_crates(stacks, instructions)
-from pprint import pprint as pp
-
-pp(stacks)
+def get_top_level_crates(input_text: str) -> str:
+    stack_data, instructions = input_text.split("\n\n")
+    stacks = populate_stacks(stack_data)
+    updated_stacks = move_crates(stacks, instructions)
+    return "".join(stack[0].strip("[]") for stack in updated_stacks)
