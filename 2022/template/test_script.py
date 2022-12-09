a7efc3ef54a9583c6a211879f07b562b4cbaa755
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-09 10:22:56 +0100

    update template

--- 2022/template/test_script.py
+++ 2022/template/test_script.py
@@ -5,21 +5,24 @@ from script import solution_part1, solution_part2
 EXERCISE_SAMPLE = """
 """.strip()
 EXERCISE_FILE_CONTENT = (Path(__file__).parent / "input.txt").read_text()
-EXPECTED_SAMPLE = 1
-EXPECTED_FILE = 1
+
+EXPECTED_SAMPLE_P1 = 1
+EXPECTED_FILE_P1 = 1
+EXPECTED_SAMPLE_P2 = 2
+EXPECTED_FILE_P2 = 2
 
 
 def test_p1_sample():
-    assert solution_part1(EXERCISE_SAMPLE) == EXPECTED_SAMPLE
+    assert solution_part1(EXERCISE_SAMPLE) == EXPECTED_SAMPLE_P1
 
 
 def test_p1_file():
-    assert solution_part1(EXERCISE_FILE_CONTENT) == 2
+    assert solution_part1(EXERCISE_FILE_CONTENT) == EXPECTED_FILE_P1
 
 
 def test_p2_sample():
-    assert solution_part2(EXERCISE_SAMPLE) == EXPECTED_FILE
+    assert solution_part2(EXERCISE_SAMPLE) == EXPECTED_SAMPLE_P2
 
 
 def test_p2_file():
-    assert solution_part2(EXERCISE_FILE_CONTENT) == 2
+    assert solution_part2(EXERCISE_FILE_CONTENT) == EXPECTED_FILE_P2
