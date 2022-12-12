85a6c9a1db785d90845f09d9c7e703ffedefec88
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-12 09:30:08 +0100

    update tests

--- 2022/day11/test_script.py
+++ 2022/day11/test_script.py
@@ -36,7 +36,7 @@ EXERCISE_FILE_CONTENT = (Path(__file__).parent / "input.txt").read_text()
 EXPECTED_SAMPLE_P1 = 10605
 EXPECTED_FILE_P1 = 64032
 EXPECTED_SAMPLE_P2 = 2713310158
-EXPECTED_FILE_P2 = 2
+EXPECTED_FILE_P2 = 12729522272
 
 
 def test_p1_sample():
@@ -47,7 +47,7 @@ def test_p1_file():
     assert solution_part1(EXERCISE_FILE_CONTENT) == EXPECTED_FILE_P1
 
 
-def test_p2_sample_smaller():
+def test_p2_sample_after_20_rounds():
     assert solution_part2(EXERCISE_SAMPLE, rounds=20) == 10197
 
 
@@ -55,5 +55,5 @@ def test_p2_sample():
     assert solution_part2(EXERCISE_SAMPLE) == EXPECTED_SAMPLE_P2
 
 
-# def test_p2_file():
-#    assert solution_part2(EXERCISE_FILE_CONTENT) == EXPECTED_FILE_P2
+def test_p2_file():
+    assert solution_part2(EXERCISE_FILE_CONTENT) == EXPECTED_FILE_P2
