cdd753eaa2a0958e2d3d3725c094cc007af60f9e
Bob Belderbos <bobbelderbos@gmail.com>
2022-12-17 08:39:49 +0100

    part 2

--- 2022/day09/script.py
+++ 2022/day09/script.py
@@ -1,43 +1,64 @@
+from typing import Iterable
+
 START_X, START_Y = 4, 0
 ADJACENT_CELL_COORDS = [
     (i, j) for i in (-1, 0, 1) for j in (-1, 0, 1) if not (i == j == 0)
 ]
 
 
-def overlaps(h_pos: list[int], t_pos: list[int]) -> bool:
-    return h_pos == t_pos
+def head_overlaps_tail(h_pos: list[int], t_pos: list[int]) -> bool:
+    return h_pos[0] == t_pos[0] and h_pos[1] == t_pos[1]
 
 
-def h_touches_t(h_pos: list[int], t_pos: list[int]) -> bool:
+def head_touches_tail(h_pos: Iterable[int], t_pos: Iterable[int]) -> bool:
     r, c = h_pos
     for x, y in ADJACENT_CELL_COORDS:
-        new = [r + x, c + y]
-        if new == t_pos:
+        new = (r + x, c + y)
+        if new == tuple(t_pos):
             return True
     return False
 
 
-def solution_part1(data: str) -> int:
-    h_pos = [START_X, START_Y]
-    t_pos = [START_X, START_Y]
-    t_visits = {tuple(t_pos)}
+def _move_sign(x: int, y: int) -> int:
+    if x > y:
+        return 1
+    elif x < y:
+        return -1
+    else:
+        return 0
+
+
+def _solution(data: str, length: int) -> int:
+    positions = [[START_X, START_Y] for _ in range(length)]
+    t_visits = {tuple(positions[-1])}
 
     for line in data.strip().splitlines():
+        print(line)
         move, steps = line.split()
 
         direction = 1 if move in ("R", "D") else -1
         x_or_y = 0 if move in ("U", "D") else 1
 
         for _ in range(int(steps)):
-            prev_h_pos = list(h_pos)
-            h_pos[x_or_y] += direction
+            positions[0][x_or_y] += direction
 
-            if not h_touches_t(h_pos, t_pos) and not overlaps(h_pos, t_pos):
-                t_pos = prev_h_pos
-                t_visits.add(tuple(t_pos))
+            for i in range(1, len(positions)):
+                head, tail = positions[i - 1], positions[i]
+
+                if not head_touches_tail(head, tail) and not head_overlaps_tail(
+                    head, tail
+                ):
+                    positions[i][0] += _move_sign(head[0], tail[0])
+                    positions[i][1] += _move_sign(head[1], tail[1])
+
+            t_visits.add(tuple(positions[-1]))
 
     return len(t_visits)
 
 
+def solution_part1(data: str) -> int:
+    return _solution(data, length=2)
+
+
 def solution_part2(data: str) -> int:
-    pass
+    return _solution(data, length=10)
--- 2022/day09/test_script.py
+++ 2022/day09/test_script.py
@@ -13,11 +13,20 @@ L 5
 R 2
 """.strip()
 EXERCISE_FILE_CONTENT = (Path(__file__).parent / "input.txt").read_text()
-
+EXERCISE_SAMPLE_2 = """
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+""".strip()
 EXPECTED_SAMPLE_P1 = 13
 EXPECTED_FILE_P1 = 6494
-EXPECTED_SAMPLE_P2 = 2
-EXPECTED_FILE_P2 = 2
+EXPECTED_SAMPLE_P2 = 36
+EXPECTED_FILE_P2 = 2691
 
 
 def test_p1_sample():
@@ -29,7 +38,11 @@ def test_p1_file():
 
 
 def test_p2_sample():
-    assert solution_part2(EXERCISE_SAMPLE) == EXPECTED_SAMPLE_P2
+    assert solution_part2(EXERCISE_SAMPLE) == 1
+
+
+def test_p2_bigger_sample():
+    assert solution_part2(EXERCISE_SAMPLE_2) == EXPECTED_SAMPLE_P2
 
 
 def test_p2_file():
